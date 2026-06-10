fbfdca95b999fa4d2f8900b3a3d702b5406cc796
kise <kaiazmi1412@gmail.com>
2026-06-10 11:04:05 +0530

    fix: removed the autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -12,23 +12,3 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt.conceallevel = 0
 	end,
 })
-
--- Refresh incline colors when colorscheme changes
-vim.api.nvim_create_autocmd("ColorScheme", {
-	callback = function()
-		-- Force reload incline with new colors
-		require("incline").setup({
-			highlight = {
-				groups = {
-					InclineNormal = { guibg = "NONE", guifg = "NONE" },
-					InclineNormalNC = { guifg = "NONE", guibg = "NONE" },
-				},
-			},
-		})
-		-- Reinitialize with correct colors
-		local color_util = require("util.colors")
-		local colors = color_util.get_incline_colors()
-		vim.api.nvim_set_hl(0, "InclineNormal", { bg = colors.bg, fg = colors.fg })
-		vim.api.nvim_set_hl(0, "InclineNormalNC", { fg = colors.fg_nc, bg = colors.bg_nc })
-	end,
-})
